ee3d3fbd4ab2adb68ee47bcfe16658b84029c6ef
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2025-05-15 16:06:56 +0530

    toggle butten resolved. close button color changed

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
             document.getElementById("res-listid").classList.toggle("res-list2");
         }
 
+
+$(".res-list ul li a").click(function(){
+    $(".res-list").toggleClass("res-list2");
+    // document.getElementById("res-listid").classList.toggle("res-list2");
+    // alert("hello");
+});
 $("#project1").click(function(){
     $("#img-contain1").toggleClass("show");
 });
